1bfac1e295508d2b5f7d6c786b09f593f37bf55b
liddictm <liddictm@uwec.edu>
2014-04-03 19:35:44 -0500

    JUnit Tests

--- project3/src/junit/JUnitTest1.java
+++
@@ -1,8 +0,0 @@
-package junit;
-
-//You must implement a test case class for each of the Transaction and Rule classes, or whatever you call these classes
-
-public class JUnitTest1 {
-	
-
-}
---
+++ project3/src/junit/TransactionTest.java
@@ -0,0 +1,75 @@
+package junit;
+
+import project3.Transaction;
+import junit.framework.TestCase;
+
+public class TransactionTest extends TestCase {
+
+	public static String testTransaction() {
+		
+		Transaction transaction = new Transaction();
+		String result = TransactionTest.testTransaction();
+		assertEquals("beercheese", result);
+		
+		return result;
+		
+	}
+
+	public void testTransactionString() {
+		
+		fail("Not yet implemented");
+	}
+
+	public void testTransactionTransaction() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	public void testContainsObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testIndexOf() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddItems() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetItems() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	public void testEqualsTransaction() {
+		fail("Not yet implemented");
+	}
+
+	public void testContainsTransaction() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
