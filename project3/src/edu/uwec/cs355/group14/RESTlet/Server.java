37a6e167123fc81a82c30fe8a2b973c13df287f5
Tyler Liddicoat <liddictm@uwec.edu>
2014-04-24 11:47:25 -0500

    Added some RESTlet stuff. It's a start haha

--- project3/src/edu/uwec/cs355/group14/RESTlet/Server.java
+++ project3/src/edu/uwec/cs355/group14/RESTlet/Server.java
@@ -2,4 +2,8 @@ package edu.uwec.cs355.group14.RESTlet;
 
 public class Server {
 
+	// receives the parameters, store the transaction set in MySQL, generate the
+	// appropriate rules, store those rules in MySQL, and return the rule set to
+	// the client system as a serializable object.
+
 }
