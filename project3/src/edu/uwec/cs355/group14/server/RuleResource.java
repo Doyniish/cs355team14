88b4d3e89ef11d873a2a7b068400bb9c371055ce
asmusjr <asmusjr@uwec.edu>
2014-05-05 12:51:31 -0500

    rough draft RESTlet

--- project3/src/edu/uwec/cs355/group14/server/RuleResource.java
+++ project3/src/edu/uwec/cs355/group14/server/RuleResource.java
@@ -6,8 +6,14 @@ import edu.uwec.cs355.group14.common.*;
 
 public interface RuleResource {
 	@Get
-	public TransactionSet retrieve();
+	public TransactionSet retrieveTS();
+	
+	public RuleSet retrieveRS();
 
 	@Put
-	public void store(TransactionSet transaction);
+	public void storeTS(TransactionSet transactionSet);
+	
+	public void storeMCL(double minimumConfidenceLevel);
+	
+	public void storeMSL(double minimumSupportLevel);
 }
--- project3/src/edu/uwec/cs355/group14/server/RuleServerResource.java
+++ project3/src/edu/uwec/cs355/group14/server/RuleServerResource.java
@@ -1,6 +1,7 @@
 package edu.uwec.cs355.group14.server;
 
 import org.restlet.resource.ServerResource;
+
 import edu.uwec.cs355.group14.common.*;
 
 // receives the parameters, store the transaction set in MySQL, generate the
@@ -9,22 +10,47 @@ import edu.uwec.cs355.group14.common.*;
 
 public class RuleServerResource extends ServerResource implements RuleResource {
 	// data
-	private double minSupportLevel;
-	private double minConfidenceLevel;
-	private static TransactionSet transactionSet = null;
+	private double minSupportLevel = -1;
+	private double minConfidenceLevel = -1;
+	private TransactionSet transactionSet = null;
+	private RuleSet ruleSet = null;
 		
 	// methods
 	public RuleServerResource () {
 		System.out.println("RuleServerResource constructor");
 	}
 
-	public TransactionSet retrieve() {
-		System.out.println("retrieve: " + RuleServerResource.transactionSet);
-		return RuleServerResource.transactionSet;
+	public TransactionSet retrieveTS() {
+		System.out.println("retrieve: " + transactionSet);
+		return transactionSet;
+	}
+	
+	public void generateRules() {
+		System.out.println(minSupportLevel);
+		System.out.println(minConfidenceLevel);
 	}
 
 	public void store (TransactionSet transactionSet) {
 		System.out.println("store: " + transactionSet);
-		RuleServerResource.transactionSet = new TransactionSet(transactionSet);
+		transactionSet = new TransactionSet(transactionSet);
+	}
+
+	public RuleSet retrieveRS() {
+		return ruleSet;
+	}
+
+	@Override
+	public void storeTS(TransactionSet transactionSet) {
+		this.transactionSet = transactionSet;
+	}
+
+	@Override
+	public void storeMCL(double minimumConfidenceLevel) {
+		this.minConfidenceLevel = minimumConfidenceLevel;
+	}
+
+	@Override
+	public void storeMSL(double minimumSupportLevel) {
+		this.minSupportLevel = minimumSupportLevel;
 	}
 }
